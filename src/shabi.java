6c1c3030948e0521d2c6803dbbe6883f2585e5e4
小仙女儿~ <2512199315@qq.com>
2019-01-08 12:01:15 +0800

    hehehe

--- src/shabi.java
+++ src/shabi.java
@@ -1,5 +1,7 @@
 public class shabi {
     public static void main(String []args){
-        System.out.println("你.......");
+        System.out.println("You are a dog.");
+        System.out.println("No.");
+        System.out.println("I am a pig.");
     }
 }
